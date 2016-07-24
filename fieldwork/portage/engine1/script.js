b7df2e66c79c1ef6e5233be0c54b06fb34cb42c1
Kelsey Ruckert <kelseyruckert@Kelseys-MacBook-Pro.local>
2016-07-24 11:14:50 -0400

    update slideshow and fix typos

--- fieldwork/portage/engine1/script.js
+++ fieldwork/portage/engine1/script.js
@@ -18,4 +18,4 @@ function ws_parallax(k,g,a){var c=jQuery;var f=c(this);var d=a.find(".ws_list");
 // Obfuscated by Javascript Obfuscator
 // http://javascript-source.com
 //***********************************************
-jQuery("#wowslider-container1").wowSlider({effect:"parallax",prev:"",next:"",duration:20*100,delay:20*100,width:640,height:360,autoPlay:true,autoPlayVideo:false,playPause:true,stopOnHover:false,loop:false,bullets:1,caption:true,captionEffect:"parallax",controls:true,controlsThumb:false,responsive:1,fullScreen:false,gestures:2,onBeforeStep:0,images:0});
+jQuery("#wowslider-container1").wowSlider({effect:"parallax",prev:"",next:"",duration:20*100,delay:65*100,width:640,height:360,autoPlay:true,autoPlayVideo:false,playPause:true,stopOnHover:false,loop:false,bullets:1,caption:true,captionEffect:"parallax",controls:true,controlsThumb:false,responsive:1,fullScreen:false,gestures:2,onBeforeStep:0,images:0});
